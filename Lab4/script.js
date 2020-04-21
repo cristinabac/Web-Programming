3bb4a32a2c8918cedd0606ddf6a329f4ba54fd53
cristinaaa <cristinabacotiu@gmail.com>
2020-04-21 18:06:33 +0300

    first commit

---
+++ Lab4/script.js
@@ -0,0 +1,39 @@
+function t1Hover(){
+
+    document.getElementById("place_big_img").style.backgroundImage = "url('1.jpg')";
+    
+}
+
+
+
+function t2Hover(){
+
+    document.getElementById("place_big_img").style.backgroundImage = "url('2.jpg')";
+    
+}
+
+
+
+function t3Hover(){
+
+    document.getElementById("place_big_img").style.backgroundImage = "url('3.jpg')";
+    
+}
+
+
+
+function t4Hover(){
+
+    document.getElementById("place_big_img").style.backgroundImage = "url('4.png')";
+    
+}
+
+
+
+function t5Hover(){
+
+    document.getElementById("place_big_img").style.backgroundImage = "url('5.jpg')";
+    
+}
+
+
